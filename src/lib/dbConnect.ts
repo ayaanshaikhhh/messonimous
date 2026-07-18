b3af962ed8c3ff4f3161f0d398908d8bffb6c46a
Ayaan Shaikh <shaikhayaan1283@gmail.com>
2026-07-18 21:43:03 +0530

    feat(database):Connected Database

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import { boolean } from "zod";
+
+type ConnectionObject = {
+    isConnected? : number
+}
+
+const connection:ConnectionObject = {}
+
+async function ConnectDB():Promise<void> {
+    if(connection.isConnected){
+        console.log("Already connected to database");
+        return
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGO_URI || "" , {
+            
+        }) 
+
+        connection.isConnected = db.connections[0].readyState
+        console.log(db);
+        console.log(connection.isConnected);
+        console.log("Database connected successfully");
+    } catch (error) {
+        console.log("Database connection failed",error);
+        process.exit()
+    }
+}
+
+export default ConnectDB

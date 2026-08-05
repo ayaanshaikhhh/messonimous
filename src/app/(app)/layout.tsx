95652f6912c5c1459c2b74daa7a53831d7a025e3
Ayaan Shaikh <shaikhayaan1283@gmail.com>
2026-08-05 17:17:14 +0530

    Moved Page and Layout into (app)

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,65 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Space_Grotesk } from "next/font/google";
+
+import "./globals.css";
+
+import AuthProvider from "@/context/AuthProvider";
+import { Toaster } from "sonner";
+
+const geistSans = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+  display: "swap",
+});
+
+const geistMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-geist-mono",
+  display: "swap",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  variable: "--font-space-grotesk",
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: "Messonimous",
+    template: "%s | Messonimous",
+  },
+  description: "Receive honest anonymous messages from anyone.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`
+        ${geistSans.variable}
+        ${geistMono.variable}
+        ${spaceGrotesk.variable}
+        h-full
+        antialiased
+      `}
+    >
+      <body className="min-h-screen bg-slate-50 font-(family-name:--font-geist-sans) text-slate-900 overflow-hidden">
+        <AuthProvider>
+          {children}
+
+          <Toaster
+            richColors
+            position="top-right"
+            closeButton
+            expand
+          />
+        </AuthProvider>
+      </body>
+    </html>
+  );
+}

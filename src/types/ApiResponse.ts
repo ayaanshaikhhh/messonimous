c0002eaf66dbff579c5c0f03af3e48423a252292
Ayaan Shaikh <shaikhayaan1283@gmail.com>
2026-08-11 17:02:55 +0530

    Fixed: TYPOS

--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -1,8 +1,8 @@
-import { Message } from "@/models/User.model"
+import {Message} from "../types/Message"
 
 export interface ApiResponse{
     success:boolean
     message:string
-    isAcceptingMessage: boolean
+    isAcceptingMessage?: boolean
     messages?: Array<Message>
 }

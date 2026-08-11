70ddf2d1ce90be729d88e57571c032169dd27e2f
Ayaan Shaikh <shaikhayaan1283@gmail.com>
2026-08-11 16:56:17 +0530

    Modified: Using Nodemailer and Our ResetPasswordEmail React Component

--- src/helpers/sendPasswordResetLink.ts
+++ src/helpers/sendPasswordResetLink.ts
@@ -1,39 +1,38 @@
-import { resend } from "@/lib/resend";
-import ResetPasswordEmail from '../../emails/PasswordResetLink';
-import { ApiResponse } from '../types/ApiResponse';
+import { transporter } from "@/lib/nodemailer";
+import { render } from "@react-email/render";
+import ResetPasswordEmail from "../../emails/PasswordResetLink";
+import { ApiResponse } from "@/types/ApiResponse";
 
 export async function sendPasswordResetLink(
-    email:string,
-    username:string,
-    resetUrl:string
+  email: string,
+  username: string,
+  resetUrl: string
 ): Promise<ApiResponse> {
-    try {
-        const {error} = await resend.emails.send({
-            from:'Messonimous <mail.messonimous@resend.dev>', 
-            to: email,
-            subject:"Messonimous Password Reset Link",
-            react:ResetPasswordEmail({username,resetUrl})
-        })
+  try {
+    const html = await render(
+      ResetPasswordEmail({
+        username,
+        resetUrl,
+      })
+    );
 
-        // If any error occurs
-        if(error){
-            return {
-                success : false,
-                message:error.message
-            }
-        }
+    await transporter.sendMail({
+      from: `"Messonimous" <${process.env.EMAIL_USER}>`,
+      to: email,
+      subject: "Reset Your Password",
+      html,
+    });
 
-        return {
-            success:true,
-            message:'Password reset link sent successfully'
-        }
+    return {
+      success: true,
+      message: "Password reset link sent successfully",
+    };
+  } catch (error) {
+    console.error("NODEMAILER PASSWORD RESET ERROR:", error);
 
-
-    } catch (error) {
-       console.error("RESEND_PASSWORD_RESET_ERROR",error) 
-       return {
-        success:false,
-        message:"Internal server error"
-       }
-    }
+    return {
+      success: false,
+      message: "Failed to send password reset email",
+    };
+  }
 }

8d0be5ee22aa4300a9fd15d3d59bc9b38d8ead38
Ayaan Shaikh <shaikhayaan1283@gmail.com>
2026-08-12 20:30:11 +0530

    Added : Send-OTP API FOR ACCOUNT DELETION

---
+++ src/app/api/delete-account/send-otp/route.ts
@@ -0,0 +1,95 @@
+import ConnectDB from "@/lib/dbConnect";
+import { AccountDeletionVerification } from "@/models/AccountDeletion.model";
+import UserModel from "@/models/User.model";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/options";
+import { sendAccountDeletionEmail } from "@/helpers/sendAccountDeletionEmail";
+
+
+export async function POST(request:Request) {
+    try {
+        await ConnectDB()
+
+        // Get Logged-In User First
+        const session = await getServerSession(authOptions);
+
+        const userId = session?.user._id
+
+        if(!session?.user._id){
+            return Response.json({
+                success:false,
+                message:"Not Authenticated"
+            },{status:401})
+        }
+
+        // Find the user
+        const user = await UserModel.findById(session.user._id);
+
+        if(!user){
+            return Response.json({
+                success:false,
+                message:"User not found"
+            },{status:404})  
+        }
+
+        // Checking if the user is scheduled for deletetion  
+        if(user.isDeleted){
+            return Response.json({
+                success:false,
+                message:"User is already scheduled for deletion."
+            },{status:400})    
+        }
+
+        // IF USER IS FOUND THEN GENERATE THE VERIFICATION CODE AND EXPIRY
+        const verificationCode = Math.floor(100000 + Math.random() * 900000).toString()
+
+        const verificationCodeExpiry = new Date(Date.now() + 10 * 60 * 1000 );
+
+        // Removing previous deletion request !
+        await AccountDeletionVerification.deleteOne({
+            userId : user._id
+        })
+
+        // Storing new delete OTP
+        await AccountDeletionVerification.create({
+            userId : user._id,
+            verificationCode,
+            verificationCodeExpiry
+        })
+
+        // Sending OTP to the registered E-mail
+        const EmailResponse = await sendAccountDeletionEmail(
+            user.email,
+            user.username,
+            verificationCode
+        )
+
+        // IF Email Response , then directly delete the OTP
+        if(!EmailResponse.success){
+            await AccountDeletionVerification.deleteOne({
+                userId: user._id
+            })
+
+            return Response.json({
+                success:false,
+                message:"Failed to send verification code"
+            },{status:500})
+        }
+
+        return Response.json({
+                success:true,
+                message:"Verification code sent successfully"
+            },{status:200})
+
+    } catch (error) {
+        console.error("SEND DELETE ACCOUNT OTP ERROR:", error);
+
+        return Response.json(
+      {
+        success: false,
+        message: "Failed to send verification code.",
+      },
+      { status: 500 }
+    );
+    }
+}

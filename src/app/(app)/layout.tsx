8b1bc2a45c85b54251fd901e76a67cbb2fac3690
Ayaan Shaikh <shaikhayaan1283@gmail.com>
2026-08-14 23:55:22 +0530

    Modified : Now it contains common wrapper for all app , removed NavBar for only selected Routes

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,14 +1,7 @@
-import Navbar from "@/components/Navbar";
-
 export default function AppLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <>
-      <Navbar />
-      <main>{children}</main>
-    </>
-  );
+  return <main>{children}</main>;
 }

2d5205257226301432d7ff93b43e53f57021acd1
Ayaan Shaikh <shaikhayaan1283@gmail.com>
2026-08-16 03:28:01 +0530

    Modified : added SessionId into Session and JWT

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,9 +1,9 @@
-import "next-auth"
+import "next-auth";
 import { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
-    interface User{
-     _id?: string;
+  interface User {
+    _id?: string;
     isVerified?: boolean;
     isAcceptingMessage?: boolean;
     username?: string;
@@ -11,11 +11,10 @@ declare module "next-auth" {
     isDeleted?: boolean;
     deletionRequestedAt?: Date | null;
     deletionScheduledFor?: Date | null;
+  }
 
-    }
-
-    interface Session {
-        user: {
+  interface Session {
+    user: {
       _id?: string;
       isVerified?: boolean;
       isAcceptingMessage?: boolean;
@@ -24,13 +23,15 @@ declare module "next-auth" {
       isDeleted?: boolean;
       deletionRequestedAt?: Date | null;
       deletionScheduledFor?: Date | null;
-        } & DefaultSession["user"]
-    }
+
+      sessionId?: string;
+    } & DefaultSession["user"];
+  }
 }
 
 declare module "next-auth/jwt" {
-    interface JWT {
-     _id?: string;
+  interface JWT {
+    _id?: string;
     isVerified?: boolean;
     isAcceptingMessage?: boolean;
     username?: string;
@@ -38,5 +39,7 @@ declare module "next-auth/jwt" {
     isDeleted?: boolean;
     deletionRequestedAt?: Date | null;
     deletionScheduledFor?: Date | null;
-    }
+
+    sessionId?: string;
+  }
 }

4770d11a341ba84ea7f348b610924abd02e969bc
Ayaan Shaikh <shaikhayaan1283@gmail.com>
2026-08-17 16:47:38 +0530

    Updated Bugs

--- src/lib/nodemailer.ts
+++ src/lib/nodemailer.ts
@@ -1,3 +1,18 @@
+// import nodemailer from "nodemailer";
+
+// export const transporter = nodemailer.createTransport({
+//   service: "gmail",
+
+//   pool: true,
+//   maxConnections: 5,
+//   maxMessages: 100,
+
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
@@ -13,4 +28,10 @@ export const transporter = nodemailer.createTransport({
   },
 });
 
-
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("❌ SMTP CONNECTION ERROR:", error);
+  } else {
+    console.log("✅ SMTP SERVER READY:", success);
+  }
+});

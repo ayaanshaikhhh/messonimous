8bb80e6711027c98ea8b9736101d4db24c4d8636
Ayaan Shaikh <shaikhayaan1283@gmail.com>
2026-08-18 19:36:46 +0530

    Minor bugs

--- src/lib/nodemailer.ts
+++ src/lib/nodemailer.ts
@@ -1,18 +1,3 @@
-// import nodemailer from "nodemailer";
-
-// export const transporter = nodemailer.createTransport({
-//   service: "gmail",
-
-//   pool: true,
-//   maxConnections: 5,
-//   maxMessages: 100,
-
-//   auth: {
-//     user: process.env.EMAIL_USER,
-//     pass: process.env.EMAIL_PASS,
-//   },
-// });
-
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
@@ -26,12 +11,4 @@ export const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-});
-
-transporter.verify((error, success) => {
-  if (error) {
-    console.error("❌ SMTP CONNECTION ERROR:", error);
-  } else {
-    console.log("✅ SMTP SERVER READY:", success);
-  }
 });
